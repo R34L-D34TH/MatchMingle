d0f9ed8c420b2dd0103bb2aca18944d3b1f8ecc5
Vaibhav Bhardwaj <vbhardwaj708@gmail.com>
2023-08-20 15:43:17 +0530

    First Commit

--- registeration.py
+++ registeration.py
@@ -11,3 +11,13 @@ web_app = Flask("MatchMingle")
 @web_app.route("/")
 def index():
     return render_template('index.html')
+
+
+def main():
+    # In order to use session object in flask, we need to set some key as secret_key in app
+    web_app.secret_key = 'MatchMingle-key-1'
+    web_app.run(port=5001)
+
+
+if __name__ == "__main__":
+    main()
